445850528cab4c4c13ffcdfade99d53e84b66f09
parasoomashkani <122097128+parasoomashkani@users.noreply.github.com>
2024-04-26 00:05:35 +0330

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
+import { db } from "@/lib/db";
+// import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  
+export default async function Home() {
 
-      
-    
-    </main>
-  );
+await db.set('hello','hello');
+
+  return <div>hello world</div>
 }
